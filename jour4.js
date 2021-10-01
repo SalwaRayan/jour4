ebecbc8213f3011c879a16bc1a6200ded57dcffe
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-01 12:03:12 +0200

    jour4

--- jour4.js
+++ jour4.js
@@ -10,7 +10,7 @@ console.log(cat)
 console.log(cat.age)
 console.log(cat["age"])
 
-if(cat.isCute = true){
+if(cat.isCute){ // = true // si puo anche non mettere perche è un boolean, la condizione va mettere true come default
   console.log("So cute!")
 }
 
@@ -49,9 +49,9 @@ console.log("")
 
 function compare(num1, num2){
   if (num1 > num2){
-    console.log(`${num1} is bigger`)
-  } else if (num2 > num1) {
-    console.log(`${num2} is bigger`)
+    console.log(`${num1} is bigger than ${num2}`)
+  } else if (num1 < num2) {
+    console.log(`${num2} is bigger than ${num1}`)
   } else {
     console.log("Both are the same")
   }
@@ -70,10 +70,12 @@ function addUp(num) {
   for (var i = 0; i <= num; i++){
     result = result + i
   }
-  console.log(result)
+  // console.log(result)
+  return result
 }
 
-addUp(12)
+var sum = addUp(12)
+console.log(sum)
 
 console.log("")
 
@@ -105,23 +107,42 @@ function generatePassword(num) {
   var randomChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   var password = ""
   for(var i = 0; i < num; i++) {
-    password = password + randomChar.charAt(Math.floor(Math.random() * randomChar.length))
+    password += randomChar.charAt(Math.floor(Math.random() * randomChar.length))
   }
-  if (password.length < 6) {
-    console.log("error")
-  } else if (password.length > 15) {
+  if (password.length < 6 || password.length > 15) {
     console.log("error")
   } else {
     console.log(password)
   }
 }
 
-generatePassword(9)
+generatePassword(4)
 
 console.log("")
 
 // Bonus II
 
 function launchDice(numberOfDice) {
-  
+  var somme = 0
+
+  for (var i = 0; i < numberOfDice; i++){
+    var dice = Math.floor(Math.random()*(6 - 1 + 1) + 1)
+    // console.log(dice)
+    somme = somme + dice
+  }
+  // console.log(somme);
+  return somme
+}
+
+var joueur1 = launchDice(15)
+var joueur2 = launchDice(15)
+
+console.log(joueur1, joueur2)
+
+if (joueur1 > joueur2) {
+  console.log(`J1 is the winner, with a score of ${joueur1}`)
+} else if (joueur1 < joueur2) {
+  console.log(`J2 is the winner, with a score of ${joueur2}`)
+} else {
+  console.log(`It's a draw!!!`)
 }
